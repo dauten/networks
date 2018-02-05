96c6d0cb8889840a96568b9f1d6a05f236a0c604
Dale Auten <dauten96@gmail.com>
2018-02-05 16:45:56 -0600

    Added inital project files.  Simple connection

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # networks
+compile both files with gcc.
+the executables should be named differently
+run 'serverout <PORT #>'
+then run 'clientout <PORT #>'
+clientout then wait for you to input IP of target
---
+++ client.c
@@ -0,0 +1,63 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+ 
+int main(int argc, char *argv[])
+{
+
+  int port;
+
+  if(argc != 2){
+	fprintf(stderr, "you forgot the port");
+  }
+  else{
+	port = atoi(argv[1]);
+	printf("yay you got the port, its %d\n", port);
+  }
+  int sockfd = 0,n = 0;
+  char recvBuff[1024];
+  struct sockaddr_in serv_addr;
+ 
+  memset(recvBuff, '0' ,sizeof(recvBuff));
+  if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0)
+    {
+      printf("\n Error : Could not create socket \n");
+      return 1;
+    }
+ 
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(port);
+  char ip[16];
+  scanf("%s",ip);
+  serv_addr.sin_addr.s_addr = inet_addr(ip);
+ 
+  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
+    {
+      printf("\n Error : Connect Failed \n");
+      return 1;
+    }
+ 
+  while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+    {
+      recvBuff[n] = 0;
+      if(fputs(recvBuff, stdout) == EOF)
+    {
+      printf("\n Error : Fputs error");
+    }
+      printf("\n");
+    }
+ 
+  if( n < 0)
+    {
+      printf("\n Read Error \n");
+    }
+ 
+  return 0;
+}
