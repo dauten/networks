240429e3417af751ce59bc2788c7eed57c2069f8
dale <dauten96@gmail.com>
2018-04-21 00:36:57 -0500

    examining what makes this sample code work to understand how to configure TLS in c

--- client.c
+++ client.c
@@ -1,175 +1,112 @@
-/**
-* Author:  Dale Auten
-* Modified: 2/14/18
-* Desc: Client Side half of a project to communicate through sockets bound to ports on a linux machine.
-* Beej's Guide to Network Programming (https://beej.us/guide/bgnet/) was referenced heavily
-* and certain lines were taken from the guide.  I have noted where.
-**/
-
-
-
-
+//SSL-Client.c
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <errno.h>
+#include <unistd.h>
+#include <malloc.h>
 #include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
-#include "openssl/bio.h"
-#include <arpa/inet.h>
-
-#include <openssl/x509v3.h>
-
-//function from beej's guide
-void *get_in_addr(struct sockaddr *sa)
-{
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
-
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+ 
+#define FAIL    -1
+ 
+int OpenConnection(const char *hostname, int port)
+{   int sd;
+    struct hostent *host;
+    struct sockaddr_in addr;
+ 
+    if ( (host = gethostbyname(hostname)) == NULL )
+    {
+        perror(hostname);
+        abort();
+    }
+    sd = socket(PF_INET, SOCK_STREAM, 0);
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = *(long*)(host->h_addr);
+    if ( connect(sd, (struct sockaddr*)&addr, sizeof(addr)) != 0 )
+    {
+        close(sd);
+        perror(hostname);
+        abort();
+    }
+    return sd;
 }
-
-int main(int argc, char *argv[])
-{
-	//declaration
-	int tcpSock, numbytes;  
-	char buf[128];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
-	char *SERVER = argv[1];
-	uint PORT = atoi(argv[2]);
-	char input[64];
-	int flag = 1;
-
-	// check if user gave needed args
-	if (argc != 3) {
-		fprintf(stderr,"usage: client <hostname> <port>\n");
-		exit(1);
-	}
-
-	struct hostent *host;
-	struct sockaddr_in addr;
-
-	if ( (host = gethostbyname(SERVER)) == NULL )
-		{
-		perror(SERVER);
-		abort();
-	}
-	tcpSock = socket(PF_INET, SOCK_STREAM, 0);
-	bzero(&addr, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(PORT);
-	addr.sin_addr.s_addr = *(long*)(host->h_addr);
-	if ( connect(tcpSock, (struct sockaddr*)&addr, sizeof(addr)) != 0 )
-	{
-		close(tcpSock);
-		perror(SERVER);
-		abort();
-	}
-
-	// create variables and load in openssl
-	SSL_CTX *context;
-	SSL *ssl;
-	SSL_library_init();
-
-	// load functions and create our context, printing error message on failure
-	OpenSSL_add_all_algorithms();
-	SSL_load_error_strings();
-	SSL_METHOD *method;
-	method = TLSv1_2_client_method();
-	if((context=SSL_CTX_new(method))==NULL) printf("context may not have been setup correctly\n");
-	
-	// create our SSL conenction using our context
-	ssl=SSL_new(context);
-	SSL_set_fd(ssl, tcpSock);
-
-	// see if we connect
-	if(SSL_connect(ssl) == -1) printf("SSL connection may have failed\n");
-	printf("If we got no errors then we are allegedly connected with %s encryption.  We will send a msg to test\n", SSL_get_cipher(ssl));
-	
-	// to double check, we will get the certificate from the server
-	X509 *cert = SSL_get_peer_certificate(ssl);
-	if(cert != NULL){
-		printf("Certificate confirmed\n");
-	}
-	else{
-		printf("Unable to obtain certificate\n");
-	}
-	
-	// send a sample message and receive/print the response
-	SSL_write(ssl, "This is being sent over TLS", 64);
-	SSL_read(ssl, input, 64);
-
-	printf("We recieved over TLS: %s\nWe should be connected and ready to send encypted messages now\n");
-
-
-	
-	send(tcpSock, "$$$$$$$$", 9, 0);
-	char* split;
-	char msg[10];
-
-
-
-
-	while(flag)
-	{
-		recv(tcpSock, input, 512, 0);
-	//	buf[numbytes] = '\0';
-		printf("client: received '%s'\n",input);
-		memcpy(msg, input, 64);
-		split = strtok(msg, " ");
-
-		if(strcmp(input, "200 BYE")==0){
-			flag=0;
-		}
-
-
-		//input
-		printf("input:\n");
-		fgets(input, 64, stdin);
-		for(int i = 0; i < 64; i++){
-			if(input[i] == '\n'){
-				input[i] = ' ';
-				input[i+1] = '\n';
-				input[i+2] = '\0';
-				i = 64;
-				break;
-			}
-		} //end for
-		if(strcmp(split, "334")==0){
-			split = strtok(input, " ");
-			BIO *bmem, *b64;
-			BUF_MEM *bptr;
-
-			b64 = BIO_new(BIO_f_base64());
-			bmem = BIO_new(BIO_s_mem());
-			b64 = BIO_push(b64, bmem);
-			BIO_write(b64, split, strlen(split));
-			BIO_flush(b64);
-			BIO_get_mem_ptr(b64, &bptr);
-			char *buff = (char *)malloc(bptr->length);
-			memcpy(buff, bptr->data, bptr->length-1);
-			buff[bptr->length-1] = 0;
-			BIO_free_all(b64);
-			sprintf(input, "%s",buff);
-		}
-
-
-		//send, recieve, and output
-		send(tcpSock, input, 100, 0);
-
-} //end while
-
-	close(tcpSock);
-
-	return 0;
-
-} //end main
-
+ 
+SSL_CTX* InitCTX(void)
+{   SSL_METHOD *method;
+    SSL_CTX *ctx;
+ 
+    OpenSSL_add_all_algorithms();  /* Load cryptos, et.al. */
+    SSL_load_error_strings();   /* Bring in and register error messages */
+    method = TLSv1_2_client_method();  /* Create new client-method instance */
+    ctx = SSL_CTX_new(method);   /* Create new context */
+    if ( ctx == NULL )
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    return ctx;
+}
+ 
+void ShowCerts(SSL* ssl)
+{   X509 *cert;
+    char *line;
+ 
+    cert = SSL_get_peer_certificate(ssl); /* get the server's certificate */
+    if ( cert != NULL )
+    {
+        printf("Server certificates:\n");
+        line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+        printf("Subject: %s\n", line);
+        free(line);       /* free the malloc'ed string */
+        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+        printf("Issuer: %s\n", line);
+        free(line);       /* free the malloc'ed string */
+        X509_free(cert);     /* free the malloc'ed certificate copy */
+    }
+    else
+        printf("Info: No client certificates configured.\n");
+}
+ 
+int main(int count, char *strings[])
+{   SSL_CTX *ctx;
+    int server;
+    SSL *ssl;
+    char buf[1024];
+    int bytes;
+    char *hostname, *portnum;
+ 
+    if ( count != 3 )
+    {
+        printf("usage: %s <hostname> <portnum>\n", strings[0]);
+        exit(0);
+    }
+    SSL_library_init();
+    hostname=strings[1];
+    portnum=strings[2];
+ 
+    ctx = InitCTX();
+    server = OpenConnection(hostname, atoi(portnum));
+    ssl = SSL_new(ctx);      /* create new SSL connection state */
+    SSL_set_fd(ssl, server);    /* attach the socket descriptor */
+    if ( SSL_connect(ssl) == FAIL )   /* perform the connection */
+        ERR_print_errors_fp(stderr);
+    else
+    {   char *msg = "Hello???";
+ 
+        printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
+        ShowCerts(ssl);        /* get any certs */
+        SSL_write(ssl, msg, strlen(msg));   /* encrypt & send message */
+        bytes = SSL_read(ssl, buf, sizeof(buf)); /* get reply & decrypt */
+        buf[bytes] = 0;
+        printf("Received: \"%s\"\n", buf);
+        SSL_free(ssl);        /* release connection state */
+    }
+    close(server);         /* close socket */
+    SSL_CTX_free(ctx);        /* release context */
+    return 0;
+}
--- server.c
+++ server.c
@@ -1,453 +1,169 @@
-/**
-* Author:  Dale Auten
-* Modified: 4/19/18
-* Desc: Server Side half of a project to communicate through sockets bound to ports on a linux machine.
-* Beej's Guide to Network Programming (https://beej.us/guide/bgnet/) was referenced heavily
-* and certain lines were taken from the guide.  I have noted where.
-**/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+//SSL-Server.c
 #include <errno.h>
+#include <unistd.h>
+#include <malloc.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <math.h>
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-#include "openssl/bio.h"
-
-#include <openssl/x509v3.h> 
-
-
-//two functions from beej's guide
-void sigchld_handler(int s)
-{
-	// waitpid() might overwrite errno, so we save and restore it:
-	int saved_errno = errno;
-
-	while(waitpid(-1, NULL, WNOHANG) > 0);
-
-		errno = saved_errno;
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <resolv.h>
+#include "openssl/ssl.h"
+#include "openssl/err.h"
+ 
+#define FAIL    -1
+ 
+int OpenListener(int port)
+{   int sd;
+    struct sockaddr_in addr;
+ 
+    sd = socket(PF_INET, SOCK_STREAM, 0);
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    if ( bind(sd, (struct sockaddr*)&addr, sizeof(addr)) != 0 )
+    {
+        perror("can't bind port");
+        abort();
+    }
+    if ( listen(sd, 10) != 0 )
+    {
+        perror("Can't configure listening port");
+        abort();
+    }
+    return sd;
 }
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
+ 
+int isRoot()
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
-
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    if (getuid() != 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+ 
 }
-
-
-	
-int main(int argc, char *args[])
+SSL_CTX* InitServerCTX(void)
+{   SSL_METHOD *method;
+    SSL_CTX *ctx;
+ 
+    OpenSSL_add_all_algorithms();  /* load & register all cryptos, etc. */
+    SSL_load_error_strings();   /* load all error messages */
+    method = TLSv1_2_server_method();  /* create new server-method instance */
+    ctx = SSL_CTX_new(method);   /* create new context from method */
+    if ( ctx == NULL )
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    return ctx;
+}
+ 
+void LoadCertificates(SSL_CTX* ctx, char* CertFile, char* KeyFile)
 {
-
-	//declarations taken from beej's guide
-	int sockfd, new_fd, numbytes;  // listen on sock_fd, new connection on new_fd
-	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr; // connector's address information
-	socklen_t sin_size;
-	struct sigaction sa;
-	int yes=1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
-
-
-	//check that the user included arguments then copy them into our ports
-	if(argc != 2){
-		printf("usage: server <tcp port>\n");
-		exit(0);
-	}
-	char *tcpPort = args[1];
-	char *udpPort = args[2];
-
-	//the structure here is beej's but I've filled it with my own logic
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
-
-	if ((rv = getaddrinfo(NULL, tcpPort, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,p->ai_protocol)) == -1) {
-			continue;
-		}
-
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,sizeof(int)) == -1) {
-			exit(1);
-		}
-
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			continue;
-		}
-
-		break;
-	}
-	freeaddrinfo(servinfo); // all done with this structure
-	if (p == NULL)  {
-		exit(1);
-	}
-
-	if (listen(sockfd, 10) == -1) {
-		perror("listen");
-		exit(1);
-	}
-	sa.sa_handler = sigchld_handler; // reap all dead processes
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		exit(1);
-	}
-
-
-	SSL_CTX *context;
-	SSL_library_init();
-
-	OpenSSL_add_all_algorithms();
-	SSL_load_error_strings();
-	if((context=SSL_CTX_new(TLSv1_2_server_method()))==NULL) printf("context may not have been setup correctly\n");
-
-	
-	if ( SSL_CTX_use_certificate_file(context, "example.crt", SSL_FILETYPE_PEM) <= 0 )
-	{
-		printf("We failed to get the certificate file\n");
-	}
-	/* set the private key from KeyFile (may be the same as CertFile) */
-	if ( SSL_CTX_use_PrivateKey_file(context, "fd.key", SSL_FILETYPE_PEM) <= 0 )
-	{
-		printf("We failed to get the key file\n");
-	}
-	/* verify private key */
-	if ( !SSL_CTX_check_private_key(context) )
-	{
-		printf("Key check failed\n");
-	}
-
-	SSL *ssl;
-
-
-
-	while(1) 
-	{  // main accept() loop
-
-		printf("main loop entered\n\n");
-		sin_size = sizeof their_addr;
-		BIO *bio;		
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1) {
-			continue;
-		}
-
-		char buf[100];
-
-		inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),s, sizeof s);
-		printf("server: got connection from %s\n", s);
-
-
-		ssl = SSL_new(context);
-		SSL_set_fd(ssl, new_fd);
-		
-		if(SSL_accept < 0) printf("We had some trouble accepting that client over SSL\n");
-
-
-		// to double check, we will get the certificate from the server
-		X509 *cert = SSL_get_peer_certificate(ssl);
-		if(cert != NULL){
-			printf("Certificate confirmed\n");
-		}
-		else{
-			printf("No certificate\n");
-		}
-
-		SSL_read(ssl, buf, 64);
-		printf("The client just sent us %s over SSL\nWe'll respond in kind\n");
-		sprintf(buf, "fuck you");
-		SSL_write(ssl, buf, 64);
-
-		printf("We sent our message using TLS and should be set up to communicate further\n");
-
-	
-
-		if (!fork()) { // this is the child process
-			close(sockfd); // child doesn't need the listener
-			printf("about to enter loop\n");
-			int state = 0; //0=default, 1=connected, 2=circle, 3=sphere, 4=cylinder
-			char* split;
-			if (send(new_fd, "201 HELO RDY", 13, 0) == -1)
-				perror("send");
-			char username[64];
-			char password[64];
-			char write[128];
-			int flag = 1;
-			while(flag){
-				numbytes = recv(new_fd, buf, 100, 0);
-				printf("recieved: %s\n", buf);
-				if(strcmp(buf, "AUTH") == 32){
-
-					send(new_fd, "334 dXNlcm5hbWU6", 50 , 0);
-					recv(new_fd, buf, 100, 0);
-					printf("recieved: '%s'\n", buf);
-					FILE *ifp, *ofp;
-					char *mode = "a+";
-					ifp = fopen(".user_pass", mode);
-					while (fscanf(ifp, "%s %s", username, password) != EOF) {
-						
-						if(strcmp(buf, username) == 0){
-							if (send(new_fd, "334 cGFzc3dvcmQ6", 50 , 0) == -1)
-								perror("send");
-							recv(new_fd, buf, 100, 0);
-							printf("recieved: '%s'\n", buf);
-							if( strcmp(buf, password)==0 ){
-								flag=0;
-								send(new_fd, "PASSWORD CORRECT, WELCOME", 50 , 0);
-								break;
-							}
-							else{
-								send(new_fd, "INVALID PASSWORD.  RETURN TO AUTH STEP.", 50 , 0);
-								flag=2;
-							}
-						}
-					  
-					}
-					if(flag == 1){
-						int r = rand();
-						r=(r%99999);
-						sprintf(write, "330 %d reconnect with AUTH again", r);
-						send(new_fd, write, 36, 0);
-						sprintf(password, "%d", r);
-						BIO *bmem, *b64;
-						BUF_MEM *bptr;
-						b64 = BIO_new(BIO_f_base64());
-						bmem = BIO_new(BIO_s_mem());
-						b64 = BIO_push(b64, bmem);
-						BIO_write(b64, password, strlen(password));
-						BIO_flush(b64);
-						BIO_get_mem_ptr(b64, &bptr);
-						char *buff = (char *)malloc(bptr->length);
-						memcpy(buff, bptr->data, bptr->length-1);
-						buff[bptr->length-1] = 0;
-						BIO_free_all(b64);
-						sprintf(write, "%s %s\n",buf, buff);
-
-						printf("We will append '%s' to user_pass\n", write);
-						fprintf(ifp, write);
-						//next we write that stuff to .user_pass
-						//functionality coming later, comrade
-					}
-					if(flag==2) flag=1;
-					fclose(ifp);
-
-
-					
-				}
-				else{
-					if (send(new_fd, "499 INAVLID FIRST COMMAND", 50 , 0) == -1)
-						perror("send");
-				}
-			}
-
-			while(1){
-				numbytes = recv(new_fd, buf, 100, 0);
-
-				buf[numbytes] = '\0';
-				printf("server: received %s", buf);
-		
-				split= strtok(buf, " "); //first token
-
-
-				if(split[0]!=0){
-					if(  strcmp(split, "HELP") == 0)
-					{
-						if (send(new_fd, "200 Select shape (CIRCLE, SPHERE, OR CYLINDER) and enter command (AREA, CIRC, VOL, RAD, HGT).", 99  , 0) == -1)
-							perror("send");
-					}
-					else if(  strcmp(split, "BYE") == 0)
-					{
-						if (send(new_fd, "200 BYE", 10 , 0) == -1)
-							perror("send");
-						flag = 0;
-					}	
-					else if(  strcmp(split, "CIRCLE") == 0)
-					{
-						if (send(new_fd, "210 CIRCLE ready", 10 , 0) == -1)
-							perror("send");
-						state = 2;
-					}
-					else if(  strcmp(split, "SPHERE") == 0)
-					{
-						if (send(new_fd, "220 SPHERE ready", 10 , 0) == -1)
-							perror("send");
-						state = 3;
-					}
-					else if(  strcmp(split, "CYLINDER") == 0)
-					{
-						if (send(new_fd, "230 CYLINDER ready", 12 , 0) == -1)
-							perror("send");
-						state = 4;
-					}
-					else if(  strcmp(split, "AREA") == 0 )
-					{
-						if(state == 2){
-
-							char* s2 = strtok(NULL, " ");
-
-							if(atoi(s2) == 0){
-								if (send(new_fd, "501-Error in args, values must be nonzero", 40 , 0) == -1)
-									perror("send");
-							}
-							else
-							{
-								sprintf(split, "250-Circle, %f", atof(s2)*atof(s2) * 3.1415);
-								if (send(new_fd, split, 40 , 0) == -1)
-									perror("send");
-							}
-						}
-						else if(state == 4){
-							char* s2 = strtok(NULL, " ");
-							char* s3 = strtok(NULL, " ");
-							printf(":%f:", atof(s2));
-							if(atoi(s2) == 0 || atoi(s3) == 0){
-								if (send(new_fd, "501-Error in args, values must be nonzero", 40 , 0) == -1)
-									perror("send");
-							}
-							else
-							{
-								sprintf(split, "250-Cylinder %f (%f)",(atof(s2)*6.283*atof(s3) + 6.283*atof(s2)*atof(s2)),atof(s3) );
-									if (send(new_fd, split, 40 , 0) == -1)
-										perror("send");
-							}
-						}
-						else
-						{
-							sprintf(split, "503-Out of Order");
-								if (send(new_fd, split, 40 , 0) == -1)
-									perror("send");
-						}
-					}
-					else if(  strcmp(split, "CIRC") == 0 )
-					{
-						if(state == 2){
-							char* s2 = strtok(NULL, " ");
-							if(atoi(s2) == 0){
-								if (send(new_fd, "501-Error in args, values must be nonzero", 40 , 0) == -1)
-									perror("send");
-							}
-							else{
-								sprintf(split, "250-Circle's Circumference %f", (sqrt(atof(s2)/3.1415) * 4/3 ) );
-								if (send(new_fd, split, 40 , 0) == -1)
-									perror("send");
-							}
-						}
-
-						else{
-							sprintf(split, "503-Out of Order");
-							if (send(new_fd, split, 40 , 0) == -1)
-								perror("send");
-						}
-					}
-					else if(  strcmp(split, "VOL") == 0 )
-					{
-						if(state == 3){
-							char* s2 = strtok(NULL, " ");
-							if(atoi(s2) == 0){
-								if (send(new_fd, "501-Error in args, values must be nonzero", 40 , 0) == -1)
-									perror("send");
-							}
-							else
-							{  
-								sprintf(split, "250-Sphere's Volume %f", (4/3 * 3.1415 * atof(s2) * atof(s2) * atof(s2)));
-								if (send(new_fd, split, 40 , 0) == -1)
-									perror("send");
-							}
-						}
-
-						else{
-							sprintf(split, "503-Out of Order");
-								if (send(new_fd, split, 40 , 0) == -1)
-									perror("send");
-						}
-					}
-					else if(  strcmp(split, "RAD") == 0 )
-					{
-						if(state == 3){
-							char* s2 = strtok(NULL, " ");
-				
-							if(atoi(s2) == 0){
-								if (send(new_fd, "501-Error in args, values must be nonzero", 40 , 0) == -1)
-									perror("send");
-							}
-							else
-							{  
-								sprintf(split, "250-Sphere's Radius %f", (1/2 * sqrt(atof(s2) / 3.1415) ));
-								if (send(new_fd, split, 40 , 0) == -1)
-									perror("send");
-							}
-						}
-
-						else
-						{
-							sprintf(split, "503-Out of Order");
-							if (send(new_fd, split, 40 , 0) == -1)
-							perror("send");
-						}
-					}
-					else if(  strcmp(split, "HGT") == 0 )
-					{
-						if(state == 4){
-							char* s2 = strtok(NULL, " ");
-							char* s3 = strtok(NULL, " ");
-							if(atoi(s2) == 0 || atoi(s3) == 0){
-								if (send(new_fd, "501-Error in args, values must be nonzero", 40 , 0) == -1)
-									perror("send");
-							}
-							else
-							{  
-								sprintf(split, "250-Cylinder's Height %f", (atof(s2) / (atof(s3) * atof(s3) * 3.1415)));
-								if (send(new_fd, split, 40 , 0) == -1)
-									perror("send");
-							}
-						}
-
-						else{
-							sprintf(split, "503-Out of Order");
-								if (send(new_fd, split, 40 , 0) == -1)
-									perror("send");
-						}
-					}
-					else if(strcmp(split, "AUTH") == 0){
-						if (send(new_fd, "200 - Already Authenticated", 50 , 0) == -1)
-							perror("send");
-
-					}
-					else
-					{
-						if (send(new_fd, "500 - Unrecognized Command or missing args", 50 , 0) == -1)
-							perror("send");
-					}//end else
-				}//end if-else-if-else
-			}//end child while
-		}//end fork
-
-
-	} //end accept while
-
-	close(new_fd);  // parent doesn't need this
-	printf("closing parent\n");
-	
-
-	return 0;
-}//end main
+    /* set the local certificate from CertFile */
+    if ( SSL_CTX_use_certificate_file(ctx, CertFile, SSL_FILETYPE_PEM) <= 0 )
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    /* set the private key from KeyFile (may be the same as CertFile) */
+    if ( SSL_CTX_use_PrivateKey_file(ctx, KeyFile, SSL_FILETYPE_PEM) <= 0 )
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    /* verify private key */
+    if ( !SSL_CTX_check_private_key(ctx) )
+    {
+        fprintf(stderr, "Private key does not match the public certificate\n");
+        abort();
+    }
+}
+ 
+void ShowCerts(SSL* ssl)
+{   X509 *cert;
+    char *line;
+ 
+    cert = SSL_get_peer_certificate(ssl); /* Get certificates (if available) */
+    if ( cert != NULL )
+    {
+        printf("Server certificates:\n");
+        line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+        printf("Subject: %s\n", line);
+        free(line);
+        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+        printf("Issuer: %s\n", line);
+        free(line);
+        X509_free(cert);
+    }
+    else
+        printf("No certificates.\n");
+}
+ 
+void Servlet(SSL* ssl) /* Serve the connection -- threadable */
+{   char buf[1024];
+    char reply[1024];
+    int sd, bytes;
+    const char* HTMLecho="<html><body><pre>%s</pre></body></html>\n\n";
+ 
+    if ( SSL_accept(ssl) == FAIL )     /* do SSL-protocol accept */
+        ERR_print_errors_fp(stderr);
+    else
+    {
+        ShowCerts(ssl);        /* get any certificates */
+        bytes = SSL_read(ssl, buf, sizeof(buf)); /* get request */
+        if ( bytes > 0 )
+        {
+            buf[bytes] = 0;
+            printf("Client msg: \"%s\"\n", buf);
+            sprintf(reply, HTMLecho, buf);   /* construct reply */
+            SSL_write(ssl, reply, strlen(reply)); /* send reply */
+        }
+        else
+            ERR_print_errors_fp(stderr);
+    }
+    sd = SSL_get_fd(ssl);       /* get socket connection */
+    SSL_free(ssl);         /* release SSL state */
+    close(sd);          /* close connection */
+}
+ 
+int main(int count, char *strings[])
+{   SSL_CTX *ctx;
+    int server;
+    char *portnum;
+ 
+    if(!isRoot())
+    {
+        printf("This program must be run as root/sudo user!!");
+        exit(0);
+    }
+    if ( count != 2 )
+    {
+        printf("Usage: %s <portnum>\n", strings[0]);
+        exit(0);
+    }
+    SSL_library_init();
+ 
+    portnum = strings[1];
+    ctx = InitServerCTX();        /* initialize SSL */
+    LoadCertificates(ctx, "auten.crt", "auten.key"); /* load certs */
+    server = OpenListener(atoi(portnum));    /* create server socket */
+    while (1)
+    {   struct sockaddr_in addr;
+        socklen_t len = sizeof(addr);
+        SSL *ssl;
+ 
+        int client = accept(server, (struct sockaddr*)&addr, &len);  /* accept connection as usual */
+        printf("Connection: %s:%d\n",inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+        ssl = SSL_new(ctx);              /* get new SSL state with context */
+        SSL_set_fd(ssl, client);      /* set connection socket to SSL state */
+        Servlet(ssl);         /* service connection */
+    }
+    close(server);          /* close server socket */
+    SSL_CTX_free(ctx);         /* release context */
+}
